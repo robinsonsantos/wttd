0cc835948852fe37b038e5087a4601d68e23d3a1
Robinson D. S. Santos <stanleymagrao@gmail.com>
2013-10-13 21:11:39 -0300

    Subscription

--- eventex/subscriptions/tests/test_views_subscribe.py
+++ eventex/subscriptions/tests/test_views_subscribe.py
@@ -2,6 +2,7 @@
 
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 class SubscribeTest(TestCase):
@@ -50,3 +51,46 @@ class SubscribeTest(TestCase):
 	form = self.resp.context['form']
 	self.assertItemsEqual(['name', 'email', 'cpf', 'phone'], form.fields)
 
+
+class SubscribePostTest(TestCase):
+    def setUp(self):
+	data = dict(name='Robinson Santos', cpf='12345678901',
+		    email='robinson@santos.com.br', phone='46-88071441') 
+	self.resp = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+	"""
+	Valid POST should redirect to /inscricao/1/
+	"""
+	self.assertEqual(302, self.resp.status_code)
+
+    def test_save(self):
+        """
+        Valid POST must be saved.
+        """
+        self.assertTrue(Subscription.objects.exists())
+
+
+class SubscribeInvalidPostTest(TestCase):
+    def setUp(self):
+	data = dict(name='Robinson Santos', cpf='000000000012',
+		    email='robinson@santos.com', phone='46-88081441')
+	self.resp = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+	"""
+	Invalid POST should not redirect.
+	"""
+	self.assertEqual(200, self.resp.status_code)
+
+    def test_form_erros(self):
+	"""
+	Form must contain erros.
+	"""
+	self.assertTrue(self.resp.context['form'].errors)
+
+    def test_dont_save(self):
+	"""
+	Do not save data.
+	"""
+	self.assertFalse(Subscription.objects.exists())
--- eventex/urls.py
+++ eventex/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'eventex.core.views.homepage', name='homepage'),
     url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
+    url(r'^inscricao/(\d+)/$', 'eventex.subscriptions.views.detail', name='detail'),
 
     # Examples:
     # url(r'^$', 'eventex.views.home', name='home'),
