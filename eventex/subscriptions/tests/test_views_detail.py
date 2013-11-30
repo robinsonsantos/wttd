4631d04c4c79989dc9d3ac37dc30f3838d77bd78
Robinson D. S. Santos <stanleymagrao@gmail.com>
2013-11-30 21:07:36 -0200

    Refactoring urls

--- eventex/subscriptions/tests/test_views_detail.py
+++ eventex/subscriptions/tests/test_views_detail.py
@@ -1,42 +1,33 @@
 # coding: utf-8
-
 from django.test import TestCase
 from eventex.subscriptions.models import Subscription
+from django.core.urlresolvers import reverse as r
 
 class DetailTest(TestCase):
     def setUp(self):
-	s = Subscription.objects.create(name='Robinson Santos', 
-			cpf='12345678901', email='robinson@santos.com',
-			phone='46-88071441')
-	self.resp = self.client.get('/inscricao/%d/' % s.pk)
+		s = Subscription.objects.create(name='Robinson Santos', 
+				cpf='12345678901', email='robinson@santos.com',
+				phone='46-88071441')
+		self.resp = self.client.get(r('subscriptions:detail', args=[s.pk]))
 
     def test_get(self):
-	"""
-	GET /inscricao/1/ should return status 200.
-	"""
-	self.assertEqual(200, self.resp.status_code)
+		'GET /inscricao/1/ should return status 200.'
+		self.assertEqual(200, self.resp.status_code)
 
     def test_template(self):
-	"""
-	Uses template
-	"""
-	self.assertTemplateUsed(self.resp, 'subscriptions/subscription_detail.html')
+		'Uses template'
+		self.assertTemplateUsed(self.resp, 'subscriptions/subscription_detail.html')
 
     def test_context(self):
-	"""
-	Context must have a subscription instance.
-	"""
-	subscription = self.resp.context['subscription']
-	self.assertIsInstance(subscription, Subscription)
+		'Context must have a subscription instance.'
+		subscription = self.resp.context['subscription']
+		self.assertIsInstance(subscription, Subscription)
 
     def test_html(self):
-	"""
-	Check if subscription data was rendered.
-	"""
-	self.assertContains(self.resp, 'Robinson Santos')
+		'Check if subscription data was rendered.'
+		self.assertContains(self.resp, 'Robinson Santos')
 
-
-class DetailNotFound(TestCase):
+class DetailViewNotFound(TestCase):
     def test_not_found(self):
-	response = self.client.get('/inscricao/0/')
-	self.assertEqual(404, response.status_code)
+		response = self.client.get(r('subscriptions:detail', args=[0]))
+		self.assertEqual(404, response.status_code)
--- eventex/subscriptions/tests/test_views_subscribe.py
+++ eventex/subscriptions/tests/test_views_subscribe.py
@@ -1,96 +1,72 @@
 # coding: utf-8
-
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
-
+from django.core.urlresolvers import reverse as r
 
 class SubscribeTest(TestCase):
-
     def setUp(self):
-	self.resp = self.client.get('/inscricao/')
+		self.resp = self.client.get(r('subscriptions:subscribe'))
 
     def test_get(self):
-	"""
-	Get /inscricao/ must return status code 200.
-	"""
-	self.assertEqual(200, self.resp.status_code)
+		'Get /inscricao/ must return status code 200.'
+		self.assertEqual(200, self.resp.status_code)
 
     def test_template(self):
-	"""
-	Response should be a rendered template.
-	"""
-	self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
+		'Response should be a rendered template.'
+		self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
 
     def test_html(self):
-	"""
-	Html must contain input controls.
-	"""
-	self.assertContains(self.resp, '<form')
-	self.assertContains(self.resp, '<input', 6)
-	self.assertContains(self.resp, 'type="text"', 4)
-	self.assertContains(self.resp, 'type="submit"')
+		'Html must contain input controls.'
+		self.assertContains(self.resp, '<form')
+		self.assertContains(self.resp, '<input', 6)
+		self.assertContains(self.resp, 'type="text"', 4)
+		self.assertContains(self.resp, 'type="submit"')
 
     def test_csrf(self):
-	"""
-	Html must contain csrf token.
-	"""
-	self.assertContains(self.resp, 'csrfmiddlewaretoken')
+		'Html must contain csrf token.'
+		self.assertContains(self.resp, 'csrfmiddlewaretoken')
 
     def test_has_form(self):
-        """
-        Context must have the subscription form.
-        """
+        'Context must have the subscription form.'
         form = self.resp.context['form']
         self.assertIsInstance(form, SubscriptionForm)
 
     def test_form_has_fields(self):
-	"""
-	Form must have 4 fields.
-	"""
-	form = self.resp.context['form']
-	self.assertItemsEqual(['name', 'email', 'cpf', 'phone'], form.fields)
+		'Form must have 4 fields.'
+		form = self.resp.context['form']
+		self.assertItemsEqual(['name', 'email', 'cpf', 'phone'], form.fields)
 
 
 class SubscribePostTest(TestCase):
     def setUp(self):
-	data = dict(name='Robinson Santos', cpf='12345678901',
+		data = dict(name='Robinson Santos', cpf='12345678901',
 		    email='robinson@santos.com.br', phone='46-88071441') 
-	self.resp = self.client.post('/inscricao/', data)
+		self.resp = self.client.post(r('subscriptions:subscribe'), data)
 
     def test_post(self):
-	"""
-	Valid POST should redirect to /inscricao/1/
-	"""
-	self.assertEqual(302, self.resp.status_code)
+		'Valid POST should redirect to /inscricao/1/'
+		self.assertEqual(302, self.resp.status_code)
 
     def test_save(self):
-        """
-        Valid POST must be saved.
-        """
+        'Valid POST must be saved.'
         self.assertTrue(Subscription.objects.exists())
 
 
 class SubscribeInvalidPostTest(TestCase):
     def setUp(self):
-	data = dict(name='Robinson Santos', cpf='000000000012',
+		data = dict(name='Robinson Santos', cpf='000000000012',
 		    email='robinson@santos.com', phone='46-88081441')
-	self.resp = self.client.post('/inscricao/', data)
+		self.resp = self.client.post(r('subscriptions:subscribe'), data)
 
     def test_post(self):
-	"""
-	Invalid POST should not redirect.
-	"""
-	self.assertEqual(200, self.resp.status_code)
+		'Invalid POST should not redirect.'
+		self.assertEqual(200, self.resp.status_code)
 
     def test_form_erros(self):
-	"""
-	Form must contain erros.
-	"""
-	self.assertTrue(self.resp.context['form'].errors)
+		'Form must contain erros.'
+		self.assertTrue(self.resp.context['form'].errors)
 
     def test_dont_save(self):
-	"""
-	Do not save data.
-	"""
-	self.assertFalse(Subscription.objects.exists())
+		'Do not save data.'
+		self.assertFalse(Subscription.objects.exists())
